55c4c6d006e8378173c741f9e012898e123a6d84
Kavindu Athaudha <kavindu@kih.com.sg>
2024-06-29 23:04:27 +1200

    Added multiple languages to CodeBlock.svelte

--- src/lib/CodeBlock.svelte
+++ src/lib/CodeBlock.svelte
@@ -1,10 +1,23 @@
-<script>
-    import { Highlight } from 'svelte-highlight';
-    import javascript from 'svelte-highlight/languages/javascript';
-    import 'svelte-highlight/styles/github.css';
-  
-    export let code = `const greet = () => { console.log('Hello, world!'); };`;
-  </script>
-  
-  <Highlight language={javascript} {code} />
-  
+<script lang="ts">
+	import { Highlight } from 'svelte-highlight';
+	import javascript from 'svelte-highlight/languages/javascript';
+	import python from 'svelte-highlight/languages/python';
+	import 'svelte-highlight/styles/github.css';
+
+	export let code: string = `const greet = () => { console.log('Hello, world!'); };`;
+	export let language: string = 'javascript';
+
+	// Define a type for the languages object
+	type LanguageMap = {
+		[key: string]: any;
+	};
+
+	// Mapping language names to their respective imports
+	const languages: LanguageMap = {
+		javascript,
+		python
+		// Add more languages as needed
+	};
+</script>
+
+<Highlight language={languages[language]} {code} />
